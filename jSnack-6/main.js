2a692d9a04332fcb687a2b8cf276aec6325f2856
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-12 17:08:58 +0100

    JSnacks ES6 part.2
    JSnack 6 completed

--- jSnack-6/main.js
+++ jSnack-6/main.js
@@ -1,9 +1,33 @@
 /**
-@file
+@file dato un array di obj iniziale, creiamo una copia dell’array di partenza
+e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+una nuova proprietà position che contiene una lettera casuale.
+Non dobbiamo modificare l’array iniziale
 @author Giuseppe Perna <giuseppeperna.cg@gmail.com>
 */
 
 // RUN CODE AFTER DOM
 $(document).ready( function() {
+
+  // CONSTANT VARIABLES
+  const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+  // Array di partenza da non modificare
+  const arrayObj = [
+    {name: 'Poppy', type: 'tshirt', color: 'red'},
+    {name: 'Jumping', type: 'occhiali', color: 'blue'},
+    {name: 'CrissCross', type: 'scarpe', color: 'black'},
+    {name: 'Jenny', type: 'borsa', color: 'pink'},
+  ];
+  console.log(arrayObj);
+
+  /*Crea un nuovo array clone del precedente ed aggiunge ad ogni oggetto
+  la proprietà position, il cui valore è una lettera casuale dell'alfabeto*/
+  const arrayObjMod = arrayObj.map(element => ({...element}));
+  for (let key in arrayObjMod) {
+      let randomChar = ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
+      arrayObjMod[key].position = randomChar;
+    }
+  console.log(arrayObjMod);
 
 })
